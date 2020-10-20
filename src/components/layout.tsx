da2104c6e417264ca3b6611958c111ec5e213b10
David Tran <davidlamt@users.noreply.github.com>
2020-10-19 21:27:55 -0700

    Make sure header is not clickable once covered

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,13 +4,16 @@ import styled from '@emotion/styled';
 
 import { Footer, Header } from './';
 
+const MainWrapper = styled.main`
+  position: relative;
+`;
+
 const ContentWrapper = styled.div`
   background-color: hsla(0, 0%, 100%, 0.9);
   box-shadow: 0 30px 50px 0 rgba(1, 1, 1, 0.15);
   margin: 15vw auto var(--spacing-0) auto;
   max-width: var(--maxWidth-wrapper);
   padding: var(--spacing-16) var(--spacing-20);
-  position: relative;
   width: 90%;
 `;
 
@@ -27,7 +30,9 @@ const Layout: React.FC<LayoutProps> = ({ children, location, title }) => {
   return (
     <div data-is-root-path={isRootPath}>
       <Header title={title} />
-      <ContentWrapper>{children}</ContentWrapper>
+      <MainWrapper>
+        <ContentWrapper>{children}</ContentWrapper>
+      </MainWrapper>
       <Footer />
     </div>
   );
