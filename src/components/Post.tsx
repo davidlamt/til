42284b75dc7339aa1ef168af5d2678f334331b73
David Tran <davidlamt@users.noreply.github.com>
2020-10-23 19:29:33 -0700

    Remove post footer nav completely when there are no other posts to link to

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -84,35 +84,35 @@ const Post: React.FC<PostProps> = ({
           </Metadata>
         </div>
         <MDXRenderer>{post.body}</MDXRenderer>
-        <hr />
-        <footer></footer>
       </article>
-      <nav>
-        <OtherPostsList
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </OtherPostsList>
-      </nav>
+      {(previous || next) && (
+        <nav>
+          <OtherPostsList
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </OtherPostsList>
+        </nav>
+      )}
     </PostContainer>
   );
 };
